01533d936bc0e1396bbbcbad353af0ec91fb91f5
Abhishek Mishra <abhishek@oreqo.io>
2024-12-09 07:56:55 +0000

    convarsion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score =33
+
+// console.log(typeof score)
+// console.log (typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+
+//"33" => 33  if give " " any value then print string type 
+// "33abc" => Nan //Not a number
+//  true => 1; if give 0 then output come false false => 0
+
+let isloggedin = ""
+let booleanIsLoggedIn = Boolean(isloggedin)
+
+// console.log(booleanIsLoggedIn)
+
+// 1 => true ;  0 => false
+//"" => false
+//"Abhi" => true
