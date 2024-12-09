701f224200952ff9b7a03f36e7ee4284a347e62c
Abhishek Mishra <abhishek@oreqo.io>
2024-12-09 07:27:57 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId =  52245
+let accountemail = "abhi@gmail.com"
+var accountPassword = "123456"
+ accountState = "Delhi"
+console.log(accountId)
+
+
+//prefered not to use var beacause of issue in block scope and functional scope 
+
+console.table([accountId,accountemail,accountPassword,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+let name = "Abhi"   //string
+let Age = 24   //number
+let isloggedin = false //boolean
+let state = null  //null type
+
+//Primitive data types
+// ---------------------------
+// number = range 2 to 53
+//bigint = Big numbers
+//string = "" 
+//boolean = true/false
+//null = standalone value or " " value
+//symbol = unique value find uniquness
+
+console.log(typeof "ABhi");   //check type what type use in js
